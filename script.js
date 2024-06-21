47bb8d4e1574d0717b94e31eca4be3c164bc3e85
Douglas Ribeiro <dgrb0325@gmail.com>
2024-06-21 15:01:09 -0300

    feat: script para home

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "admin") {
+      window.location.href = "/home/admin/admin.html";
+    } else {
+      alert(
+        "Usuário ou senha incorretos!\nDica\nUsuário: admin\nSenha: admin "
+      );
+    }
+  });
+});
